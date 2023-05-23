e2324afd4611792779ac2b2e85c0f5cf0e2a0e0d
Egyed József <jegyed50@gmail.com>
2023-05-23 16:24:55 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-basic.showIcon(IconNames.Heart)
+let Y = 0
+let X = 0
+let szorzo = 3
+music.setVolume(255)
 basic.forever(function () {
-	
+    X = pins.analogReadPin(AnalogPin.P0)
+    serial.writeValue("x", X)
+    Y = pins.analogReadPin(AnalogPin.P1)
+    serial.writeValue("y", Y)
+    basic.pause(250)
+    music.playSoundEffect(music.createSoundEffect(
+    WaveShape.Sine,
+    220 + X * szorzo,
+    220 + Y * szorzo,
+    255,
+    0,
+    250,
+    SoundExpressionEffect.None,
+    InterpolationCurve.Linear
+    ), SoundExpressionPlayMode.UntilDone)
 })
